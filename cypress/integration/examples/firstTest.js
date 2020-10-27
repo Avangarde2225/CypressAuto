2190a7df1a5485daa70422bab58da5084b99ca90
Serdar <serdar2225@gmail.com>
2020-10-26 20:16:14 -0400

    click method

--- cypress/integration/examples/firstTest.js
+++ cypress/integration/examples/firstTest.js
@@ -9,8 +9,8 @@ describe('My First Test', function() {
       cy.get(".product:visible").should("have.length",4)
       cy.get(".products").find(".product").should("have.length",4)
 
-      cy.get(".products").find(".product").eq(2).contains("ADD TO CART")
-      cy.contains("ADD TO CART")
+      cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click()
+     // cy.contains("ADD TO CART")
 
       
     })
