01aa74358bdc488fe7324c7e4e4477d0ba5f5761
Serdar <serdar2225@gmail.com>
2020-10-28 21:46:35 -0400

    second test

--- cypress/integration/examples/secondTest.js
+++ cypress/integration/examples/secondTest.js
@@ -1,23 +1,15 @@
 /// <reference types = "Cypress" />
 
-describe('My First Test', function() {
+describe('My Second Test', function() {
     it('Does not do much!', function() {
 
     
       cy.visit("https://www.rahulshettyacademy.com/seleniumPractise/#/")
       cy.get(".search-keyword").type("ca")
-      cy.get(".product:visible").should("have.length",4)
+      
 
       cy.get(".products").as("productLocator")
 
-      cy.get("@productLocator").find(".product").should("have.length",4)
-      
-      cy.get("@productLocator").find(".product").eq(2).contains("ADD TO CART").click().then(function() {
-
-        console.log('sf')
-      })
-
-     
      // cy.contains("ADD TO CART")
       cy.get("@productLocator").find(".product").each(($e1, index, $list) => {
 
@@ -27,15 +19,10 @@ describe('My First Test', function() {
         $e1.find("button").trigger("click")
       }
      })
-     cy.get(".brand").then(function (logo) {
-
-      cy.log(logo.text())
-     })
-      
-
-
-
-
+     cy.get('.cart-icon > img').click()
+     cy.contains('PROCEED TO CHECKOUT').click()
+     cy.contains('Place Order').click()
+    
       
     })
   })
