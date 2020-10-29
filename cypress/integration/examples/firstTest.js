c8e231b59fcc1d52271b526801a38eb43e7ed1c9
Serdar <serdar2225@gmail.com>
2020-10-28 21:20:44 -0400

    as method

--- cypress/integration/examples/firstTest.js
+++ cypress/integration/examples/firstTest.js
@@ -7,11 +7,16 @@ describe('My First Test', function() {
       cy.visit("https://www.rahulshettyacademy.com/seleniumPractise/#/")
       cy.get(".search-keyword").type("ca")
       cy.get(".product:visible").should("have.length",4)
-      cy.get(".products").find(".product").should("have.length",4)
 
-      //cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click()
+      cy.get(".products").as("productLocator")
+
+      cy.get("@productLocator").find(".product").should("have.length",4)
+      
+      cy.get("@productLocator").find(".product").eq(2).contains("ADD TO CART").click()
+
+      console.log('sf')
      // cy.contains("ADD TO CART")
-      cy.get(".products").find(".product").each(($e1, index, $list) => {
+      cy.get("@productLocator").find(".product").each(($e1, index, $list) => {
 
       const textVeg = $e1.find("h4.product-name").text()
       if(textVeg.includes("Cashews")){
