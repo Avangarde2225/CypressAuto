4f44551a13a722fc1cab26a103ea0a0724fd5096
Serdar <serdar2225@gmail.com>
2020-10-30 00:28:37 -0400

    dynamic checkbox

--- cypress/integration/examples/thirdTest.js
+++ cypress/integration/examples/thirdTest.js
@@ -8,13 +8,24 @@ describe('My Third Test', function() {
       cy.get("#checkBoxOption1").check().should("be.checked").and("have.value","option1")
       cy.get("#checkBoxOption1").uncheck().should("not.be.checked")
       cy.get('input[type="checkbox"]').check(["option2", "option3"])
-     
-     })
+
+
+      cy.get("select").select('option2').should("have.value", "option2")
       
+      // dynamic dropdown
 
+      cy.get("#autocomplete").type("ind")
 
+      cy.get(".ui-menu-item div").each(($e1, index, $list) => {
 
-
-      
+        if($e1.text() === "India") {
+        $e1.click()
+      }
     })
+
+      cy.get("#autocomplete").should("have.value", "India")
+     
+    
+    })
+  })
  
