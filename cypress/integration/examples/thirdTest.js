53342f1b2dd17961fd21d5f97e2c56cd95ae0990
Serdar <serdar2225@gmail.com>
2020-10-29 23:27:57 -0400

    thirdtest

---
+++ cypress/integration/examples/thirdTest.js
@@ -0,0 +1,19 @@
+/// <reference types = "Cypress" />
+
+describe('My Third Test', function() {
+    it('Does not do much!', function() {
+
+    
+      cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/")
+      cy.get("#checkBoxOption1").click()
+      
+     
+     })
+      
+
+
+
+
+      
+    })
+ 
