5375b8b6ed1cdb18341d6998ebfebf79df214390
Serdar <serdar2225@gmail.com>
2020-10-29 23:59:05 -0400

    check method

--- cypress/integration/examples/thirdTest.js
+++ cypress/integration/examples/thirdTest.js
@@ -5,8 +5,9 @@ describe('My Third Test', function() {
 
     
       cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/")
-      cy.get("#checkBoxOption1").click()
-      
+      cy.get("#checkBoxOption1").check().should("be.checked").and("have.value","option1")
+      cy.get("#checkBoxOption1").uncheck().should("not.be.checked")
+      cy.get('input[type="checkbox"]').check(["option2", "option3"])
      
      })
       
