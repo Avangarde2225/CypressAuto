bfa2410817dfadcf178d18ace4b08c9307c4b916
Serdar <serdar2225@gmail.com>
2020-10-31 23:50:38 -0400

    fourth Test

---
+++ cypress/integration/fourthTest.js
@@ -0,0 +1,12 @@
+/// <reference types = "Cypress" />
+
+describe('My Fourth Test', function() {
+    it('Does not do much!', function() {
+
+    
+      cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/")
+      cy.get("")   
+    
+    })
+  })
+ 
