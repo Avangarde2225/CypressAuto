5a9d925500f724b699defea0677739cf022b2b28
Serdar <serdar2225@gmail.com>
2020-11-02 19:16:57 -0500

    7th test

---
+++ cypress/integration/examples/7thTest.js
@@ -0,0 +1,15 @@
+/// <reference types = "Cypress" />
+
+describe('My seventh Test', function() {
+    it('Does not do much!', function() {
+
+    
+      cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/")
+    
+      
+
+
+
+  })
+ 
+})
