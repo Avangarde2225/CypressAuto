fe1e0c38be08b59ba05d0cb108116c37136443dd
Serdar <serdar2225@gmail.com>
2020-11-21 02:56:54 -0500

    more methods

--- cypress/integration/PageObjects/HomePage.js
+++ cypress/integration/PageObjects/HomePage.js
@@ -5,6 +5,22 @@ class HomePage{
         cy.get('input[name = "name"]:nth-child(2)')
     }
 
+    getTwoWayDataBindin(){
+
+        return cy.get(':nth-child(4) > .ng-untouched')
+    }
+
+    getGender(){
+        return cy.get('select')
+    }
+    getEntrepreneur(){
+        cy.get('#inlineradio3')
+    }
+
+    getShopTab(){
+        return cy.get(":nth-child(2) > .nav-link")
+    }
+
 }
 
 export default HomePage;
